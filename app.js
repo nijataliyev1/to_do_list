0db32dc0a091eba6debae79a4f345250d8d6fc68
nijataliyev1 <nicat3699@gmail.com>
2023-10-31 22:29:52 +0400

    update

--- app.js
+++ app.js
@@ -19,15 +19,44 @@ function addNewItem (parent,order,content,add) {
     })
     if (add) {
         parent.insertBefore(item,parent.children[parent.children.length - 1]);
+        event.target.value = "";
     }
     return item;
 }
 
-let listFooter = document.querySelector(".list-footer");
+function getInputContent() {
+    return document.querySelector(".list-footer input").value;
+}
 
-console.log(listFooter);
+document.querySelector(".list-footer input").addEventListener("keyup", event => {
+    if (getInputContent().split(" ").length - 1 != getInputContent().length && event.key == "Enter") {
+        addNewItem(document.querySelector(".container").children[3],order++,event.target.value,true);
+        event.target.parentElement.classList.add("display-none");
+    }
+})
 
+document.querySelector(".list-footer img").addEventListener("mouseover", event => {
+    event.target.src = "./images/cancel_hover.svg"
+})
 
-let container = document.querySelector(".container").children[3];
-addNewItem(container,order++,"Task3",true);
-addNewItem(container,order++,"Task3",true);
+document.querySelector(".list-footer img").addEventListener("mouseleave", event => {
+    event.target.src = "./images/cancel.svg"
+})
+
+document.querySelector(".list-footer img").addEventListener("click", event => {
+    event.target.parentElement.children[0].value = "";
+})
+
+document.querySelector(".add-button").addEventListener("click", event => {
+    let listFooter = document.querySelector(".list-footer");
+    let input = listFooter.children[0];
+    let isDisplayNone = listFooter.classList.contains("display-none");
+    if (isDisplayNone) {
+        listFooter.classList.remove("display-none")
+    }
+    else if (input.value.split(" ").length - 1 != input.value.length) {
+        addNewItem(document.querySelector(".container").children[3],order++,input.value,true);
+        input.parentElement.classList.add("display-none");
+        input.value = "";
+    }
+})
